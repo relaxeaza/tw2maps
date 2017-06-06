8c3b56c084eed0b84f5af3cd026389502f431c4f
mafrazz <mafrazzrafael@gmail.com>
2017-06-06 15:55:21 -0300

    prepare share map feature

--- tw2map-viewer.js
+++ tw2map-viewer.js
@@ -1,17 +1,3 @@
-function ajaxGet (url, callback) {
-    var xhr = new XMLHttpRequest()
-    
-    xhr.open('GET', url, 1)
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-    
-    xhr.onreadystatechange = function () {
-        xhr.readyState > 3 && callback(xhr.responseText, xhr)
-    }
-
-    xhr.send()
-}
-
 var isMobile = (function () {
     var check = false
     ;(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera)
@@ -218,27 +204,21 @@ var mapController = (function () {
 
             window.addEventListener('mousewheel', function (e) {
                 if (e.wheelDeltaY > 0) {
-                    if (villageSize >= 10) {
-                        return
-                    }
-
-                    villageSize++
+                    zoom('in')
                 } else {
-                    if (villageSize <= 1) {
-                        return
-                    }
-
-                    villageSize--
+                    zoom('out')
                 }
-
-                console.log(villageSize)
-
-                villageBlock = villageSize + villageMargin
-                currentPosition.x = centerCoordX * villageBlock
-                currentPosition.y = centerCoordY * villageBlock
-
-                renderCache()
             })
+
+            if (isMobile) {
+                document.getElementById('zoomIn').addEventListener('touchstart', function() {
+                    zoom('in')
+                })
+
+                document.getElementById('zoomOut').addEventListener('touchstart', function() {
+                    zoom('out')
+                })
+            }
         }
 
         initialized = true
@@ -277,6 +257,58 @@ var mapController = (function () {
         }
 
         window.requestAnimationFrame(renderLoop)
+    }
+
+    function zoomMobile (to) {
+        if (to === 'in') {
+            if (villageSize >= 8) {
+                return
+            }
+
+            villageSize++
+        } else {
+            if (villageSize <= 1) {
+                return
+            }
+
+            villageSize--
+        }
+    }
+
+    function zoomDesktop (to) {
+        if (to === 'in') {
+            if (villageSize >= 6) {
+                return
+            }
+
+            villageSize++
+        } else {
+            if (villageSize <= 1) {
+                return
+            }
+
+            villageSize--
+        }
+    }
+
+    function zoom (to) {
+        if (isMobile) {
+            zoomMobile(to)
+        } else {
+            zoomDesktop(to)
+        }
+
+        if (villageSize < 2) {
+            villageMargin = 0
+        } else {
+            villageMargin = 1
+        }
+
+        villageBlock = villageSize + villageMargin
+        currentPosition.x = centerCoordX * villageBlock
+        currentPosition.y = centerCoordY * villageBlock
+
+        renderCache()
     }
 
     function freeze () {
@@ -566,7 +598,8 @@ var mapController = (function () {
     }
 
     function clearBase () {
-        baseCtx.clearRect(0, 0, baseCanvas.width, baseCanvas.height)
+        baseCtx.fillStyle = '#436213'
+        baseCtx.fillRect(0, 0, baseCanvas.width, baseCanvas.height)
     }
 
     function clearOverlay () {
@@ -588,18 +621,42 @@ var mapController = (function () {
         delete customColors[category][id.toLowerCase()]
     }
 
+    function uploadMap () {
+        var image64 = baseCanvas.toDataURL().replace('data:image/png;base64,', '')
+        var headers = {
+            'Authorization': 'Client-ID 251edbf78dd6ffc',
+            'Cache-Control': null,
+            'X-Requested-With': null
+        }
+
+        var xhr = qwest.post('https://api.imgur.com/3/image', {
+            image: image64
+        }, {
+            headers: headers
+        })
+
+        xhr.then(function (xhr, request) {
+            console.log(request.data.link)
+        })
+
+        xhr.catch(function (xhr, request) {
+            console.log('Ocorreu algum erro.')
+        })
+    }
+
     return {
         init: init,
         renderBase: renderBase,
         renderOverlay: renderOverlay,
         renderCache: renderCache,
         addCustom: addCustom,
-        removeCustom: removeCustom
+        removeCustom: removeCustom,
+        uploadMap: uploadMap
     }
 })()
 
-ajaxGet('br20.json', function (data) {
-    DATA = JSON.parse(data)
+qwest.get('br20.json').then(function (data) {
+    DATA = JSON.parse(data.response)
 
     document.querySelector('#loading').style.display = 'none'
 
@@ -611,7 +668,5 @@ ajaxGet('br20.json', function (data) {
     customController.add('ACD', '#00a0f4')
     customController.add('ACA', '#00a0f4')
     customController.add('U.M', 'red')
-    // mapController.renderCache()
-
-
+    mapController.renderCache()
 })
